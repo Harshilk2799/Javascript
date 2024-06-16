b89be0e8d661bb762372d6effd0d2fc9fa8f4747
Harshil Khatri <khatriharshil26@gmail.com>
2024-06-16 22:30:53 +0530

    Implementation of Function in javascript

---
+++ lecture12/function.js
@@ -0,0 +1,51 @@
+// Function definition
+// function sayHello() {
+//   console.log("Hello World");
+// }
+// Function use - function call
+// sayHello();
+
+// function printCouting() {
+//   for (let i = 1; i <= 100; i++) {
+//     console.log(i);
+//   }
+// }
+// printCouting();
+
+// Pass Parameter
+// function printNumber(num) {
+//   console.log("Printing Number: ", num);
+// }
+// // Function Call - Value Argument
+// printNumber(5);
+
+// function sum(num1, num2) {
+//   console.log(num1 + num2);
+// }
+// sum(5, 6);
+
+// return function
+// function getSum(num1, num2, num3) {
+//   let sum = num1 + num2 + num3;
+//   return sum;
+// }
+// let result = getSum(5, 6, 8);
+// console.log(result);
+
+// function getMyName(firstName, lastName) {
+//   let fullName = firstName + " " + lastName;
+//   return fullName;
+//   //   unreachable statements
+//   // let a = 5;
+//   // let b = 5;
+//   // let sum = a+b
+//   // console.log(sum)
+// }
+// let result = getMyName("Harshil", "Khatri");
+// console.log(result);
+
+// Anonymous Functions
+let getMultiplication = function (a, b) {
+  return a * b;
+};
+console.log(getMultiplication(5, 6));
