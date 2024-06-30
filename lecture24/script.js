d687efd94db8257d49728e8250f111d09e22cf70
Harshil Khatri <khatriharshil26@gmail.com>
2024-06-30 17:53:54 +0530

    Implementation of BOM - Navigator in javascript

---
+++ lecture24/script.js
@@ -0,0 +1,9 @@
+console.log(navigator);
+console.log(navigator.userAgent);
+console.log(navigator.cookieEnabled);
+console.log(navigator.credentials);
+console.log(navigator.languages);
+console.log(navigator.appCodeName);
+console.log(navigator.appVersion);
+console.log(navigator.platform);
+console.log(navigator.userActivation);
