43c10030969ea8ae6bce29f796aaa793c162a632
Harshil Khatri <khatriharshil26@gmail.com>
2024-06-30 17:48:12 +0530

    Implementation of BOM - Location in javascript

---
+++ lecture23/script.js
@@ -0,0 +1,14 @@
+console.log(location.href);
+console.log(location.pathname);
+console.log(location.protocol);
+console.log(location.port);
+console.log(location.search);
+console.log(location.host);
+console.log(location.hostname);
+console.log(location.origin);
+
+function fun() {
+  location.assign(
+    "https://claude.ai/chat/7f881de2-358c-4c8d-95bc-170a7657ad4d"
+  );
+}
