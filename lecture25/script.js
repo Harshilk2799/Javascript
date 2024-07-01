e64ea277daea4614498d8f562872c7b156b379f3
Harshil Khatri <khatriharshil26@gmail.com>
2024-07-01 20:40:48 +0530

    Implementation of Arrow Function in javascript

---
+++ lecture25/script.js
@@ -0,0 +1,19 @@
+// Arrow Function
+// => Arrow functions are a concise way to write function expressions in JavaScript, introduced in ECMAScript 6 (ES6).
+// => They provide a shorter syntax compared to traditional function expressions.
+
+// arrow function
+// let sum = (num1, num2) => {
+//   return num1 + num2;
+// };
+// console.log(sum(45, 12));
+
+// Short way of arrow function
+// let sum = (num1, num2) => num1 + num2;
+// console.log(sum(45, 12));
+
+// Arrow function with no parameters
+let msg = () => console.log("Hello World");
+msg();
+
+console.log("Type of ", typeof msg);
