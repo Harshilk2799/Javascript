ec51b25f9d59b47903f4602037d006f6e084789b
Harshil Khatri <khatriharshil26@gmail.com>
2024-07-06 12:41:20 +0530

    Implementation of Spread Operator in javascript

---
+++ lecture30/spread_operator.js
@@ -0,0 +1,49 @@
+// Spread Operator : Unpacks Elements
+
+// Copy or Clone
+// const numbers = [1, 2, 3];
+// const newNumbers = [...numbers];
+// console.log("Numbers: ", numbers);
+// console.log("New Numbers: ", newNumbers);
+
+// Merge Array
+// const num1 = [1, 2, 3];
+// const num2 = [4, 5, 6];
+// const final = [...num1, ...num2];
+// console.log(final);
+
+// Compose
+// const num1 = [1, 2, 3];
+// const num2 = [4, 5, 6];
+// const final = [100, ...num1, ...num2, 200];
+// console.log(final);
+
+// String
+// const fullName = "Harshil Khatri";
+// console.log([...fullName]);
+
+// Copy Object
+// const person = {
+//   name: "Harshil",
+//   age: 25,
+//   city: "Ahmedabad",
+// };
+
+// const newPerson = { ...person };
+// console.log(newPerson);
+
+// Merge Object
+// const user = {
+//   name: "Harshil",
+// };
+// const address = {
+//   city: "Ahmedabad",
+//   state: "Gujarat",
+// };
+
+// const newUser = {
+//   ...user,
+//   ...address,
+// };
+
+// console.log(newUser);
