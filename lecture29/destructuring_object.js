1b83b436ea0b3a713228700254f1a10b58d51abb
Harshil Khatri <khatriharshil26@gmail.com>
2024-07-06 12:41:00 +0530

    Implementation of Destructuring Object in javascript

---
+++ lecture29/destructuring_object.js
@@ -0,0 +1,11 @@
+// Destructuring Object
+obj = {
+  name: "Harshil",
+  age: 25,
+  city: "Ahmedabad",
+};
+
+let { name, age, city } = obj;
+console.log(name);
+console.log(age);
+console.log(city);
