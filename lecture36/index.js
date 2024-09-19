e7631b18fca589d2d3478ec7a0b4fb235f97998c
Harshil Khatri <khatriharshil26@gmail.com>
2024-09-19 23:21:19 +0530

    Implementation of strict mode in javascript

---
+++ lecture36/index.js
@@ -0,0 +1,7 @@
+// strict mode
+// => Strict mode in JavaScript is a feature that enables a stricter parsing and error handling for your code.
+// => When you enable strict mode, it helps you write cleaner code and catch errors that might otherwise go unnoticed.
+
+"use strict";
+a = 10;
+console.log(a);
